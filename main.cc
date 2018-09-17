b189a36eb29b03d2893f3306fa550d5923c96ec0
Haoran Wang <vitowang1998@gmail.com>
2018-09-17 16:42:33 -0400

    Updates main.cc file.

---
+++ main.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1;
+    string str2;
+
+    cout << "Please input the first string: " << endl;
+    cin >> str1;
+
+    cout << "Please input the second string: " << endl;
+    cin >> str2;
+
+    if (str1 == str2) {
+        cout << "The two strings are equal." << endl;
+    } else {
+        cout << "The two strings are not equal." << endl;
+    }
+
+    return 0;
+}
